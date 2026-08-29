bf167c0faeb1160004fb296b5f0dd0689f3da07c
traique <95959617+traique@users.noreply.github.com>
2026-08-29 13:49:17 +0700

    fix: add root dashboard redirect

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from fastapi.responses import RedirectResponse
+
 from faable.app import app
+
+
+@app.get("/", include_in_schema=False)
+def root() -> RedirectResponse:
+    return RedirectResponse(url="/auth", status_code=307)
+
 
 __all__ = ["app"]
